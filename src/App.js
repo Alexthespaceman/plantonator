34934532ebcca4a64a607ab85087369933524eea
Alex Tristram <allex.tristram@gmail.com>
2023-03-30 16:49:33 +0100

    added state to app and styles to app.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header.jsx";
+import pic1 from "./images/cactus.jpeg";
+import pic2 from "./images/gerbera.jpeg";
+import pic3 from "./images/golden-pathos.jpeg";
+import pic4 from "./images/monstera.jpeg";
+import pic5 from "./images/portulacaria-afra.jpeg";
+import pic6 from "./images/snake-plant.jpeg";
+import pic7 from "./images/succulent.jpeg";
+import pic8 from "./images/viola.jpeg";
+import pic9 from "./images/yucca.jpeg";
+import Menu from "./Menu.jsx";
+import Plants from "./Plants";
+import Reset from "./Reset";
+import Start from "./Start";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    plants: [
+      {
+        name: "Golden Pathos",
+        img: pic1,
+        total: 9,
+        id: 1,
+      },
+
+      {
+        name: "cactus",
+        img: pic2,
+        total: 5,
+        id: 2,
+      },
+      {
+        name: "Gerbera",
+        img: pic3,
+        total: 7,
+        id: 3,
+      },
+      {
+        name: "Monstera",
+        img: pic4,
+        total: 8,
+        id: 4,
+      },
+      {
+        name: "Portulacaria Afra",
+        img: pic5,
+        total: 6,
+        id: 5,
+      },
+
+      {
+        name: "Snake Plant",
+        img: pic6,
+        total: 4,
+        id: 6,
+      },
+      {
+        name: "Succulent",
+        img: pic7,
+        total: 8,
+        id: 7,
+      },
+      {
+        name: "Viola",
+        img: pic8,
+        total: 2,
+        id: 8,
+      },
+      {
+        name: "Yucca",
+        img: pic9,
+        total: 2,
+        id: 9,
+      },
+    ],
+    count: 0,
+  };
+
+  plusOne = () => {
+    console.log("invoked");
+    if (this.state.count < 6) {
+      this.setState((prevState) => {
+        return { count: prevState.count + 1 };
+      });
+    }
+    console.log(this.state.count);
+  };
+
+  plusTwo = () => {
+    if (this.state.count < 6) {
+      this.setState((prevState) => {
+        return { count: prevState.count + 2 };
+      });
+    }
+    console.log(this.state.count);
+  };
+
+  plusThree = () => {
+    if (this.state.count < 6) {
+      this.setState((prevState) => {
+        return { count: prevState.count + 3 };
+      });
+    }
+    console.log(this.state.count);
+  };
+
+  reset = () => {
+    this.setState({ count: 0 });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Plants
+          key={this.state.plants.id}
+          plants={this.state.plants}
+          count={this.state.count}
+          plantTotal={this.state.plants.total}
+        />
+        <Menu
+          plusOne={this.plusOne}
+          plusTwo={this.plusTwo}
+          plusThree={this.plusThree}
+        />
+        <Reset reset={this.reset} />
+        <Start plusOne={this.plusOne} />
+      </div>
+    );
+  }
 }
 
 export default App;
