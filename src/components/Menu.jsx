df9df8fd5e5578391240d5d057564ccacd8b8acb
Alex Tristram <allex.tristram@gmail.com>
2023-04-13 23:01:24 +0100

    added disabled bools to each button and added each respective new function

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,19 +4,19 @@ function Menu(props) {
   return (
     <div className="menu-buttons">
       <p>How much water do you want your plant to have?</p>
-      <button  className="menu-button"  onClick={props.plusOne}>
+      <button  className="menu-button"  onClick={props.plusOneFirstRow} disabled={props.firstRowDisabled}>
         Small
       </button>
-      <button  className="menu-button" onClick={props.plusTwo}>Medium</button>
-      <button  className="menu-button" onClick={props.plusThree}>Large</button>
+      <button  className="menu-button" onClick={props.plusTwoFirstRow} disabled={props.firstRowDisabled}>Medium</button>
+      <button  className="menu-button" onClick={props.plusThreeFirstRow} disabled={props.firstRowDisabled}>Large</button>
       <p>How much light do you want your plant to have?</p>
-      <button  className="menu-button" onClick={props.plusOne}>Small</button>
-      <button  className="menu-button" onClick={props.plusTwo}>Medium</button>
-      <button  className="menu-button" onClick={props.plusThree}>Large</button>
+      <button  className="menu-button" onClick={props.plusOneSecondRow} disabled={props.secondRowDisabled}>Small</button>
+      <button  className="menu-button" onClick={props.plusTwoSecondRow} disabled={props.secondRowDisabled}>Medium</button>
+      <button  className="menu-button" onClick={props.plusThreeSecondRow} disabled={props.secondRowDisabled}>Large</button>
       <p>How big do you want your plant to be?</p>
-      <button  className="menu-button" onClick={props.plusOne}>Small</button>
-      <button  className="menu-button" onClick={props.plusTwo}>Medium</button>
-      <button  className="menu-button"onClick={props.plusThree}>Large</button>
+      <button  className="menu-button" onClick={props.plusOneThirdRow} disabled={props.thirdRowDisabled}>Small</button>
+      <button  className="menu-button" onClick={props.plusTwoThirdRow} disabled={props.thirdRowDisabled}>Medium</button>
+      <button  className="menu-button"onClick={props.plusThreeThirdRow} disabled={props.thirdRowDisabled}>Large</button>
       <div className="main-buttons">
         <button className="start" onClick={props.reset}>
           Reset
